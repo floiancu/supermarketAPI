54afff965ee8a3f9655fe464cbd2453718d99305
Florian Iancu <florianiancu@ymail.com>
2025-09-09 12:50:49 +0300

    Extra test cases.

--- src/test/java/com/supermarket/controller/ClientControllerTest.java
+++ src/test/java/com/supermarket/controller/ClientControllerTest.java
@@ -1,11 +1,13 @@
 package com.supermarket.controller;
 
 import com.supermarket.dto.CheckoutResponse;
+import com.supermarket.model.Item;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.List;
+import java.util.UUID;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ClientControllerTest {
@@ -15,12 +17,41 @@ class ClientControllerTest {
             .build();
 
     @Test
-    void checkout_ok(){
+    void checkout_whenItemsExist_ok(){
         webTestClient.post()
                 .uri("/checkout")
                 .bodyValue(List.of("apple", "banana"))
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(CheckoutResponse.class).isEqualTo(new CheckoutResponse(40));
+    }
+
+    @Test
+    void checkout_whenMultipleQuantity_ok(){
+        webTestClient.post()
+                .uri("/checkout")
+                .bodyValue(List.of("apple", "apple", "apple"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(CheckoutResponse.class).isEqualTo(new CheckoutResponse(30));
+    }
+
+    @Test
+    void checkout_whenItemDoesNotExist_badRequest(){
+        webTestClient.post()
+                .uri("/checkout")
+                .bodyValue(List.of("apple", "kumquat"))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody(String.class).isEqualTo("Item(s) not found: kumquat");
+    }
+
+    @Test
+    void checkout_whenRequestBodyDoesNotMatch_badRequest(){
+        webTestClient.post()
+                .uri("/checkout")
+                .bodyValue(new Item(UUID.randomUUID(), "Apple", 10.0, null))
+                .exchange()
+                .expectStatus().isBadRequest();
     }
 }
